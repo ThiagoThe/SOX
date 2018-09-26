bf6658003f3f1da59a4e8d9e6e8d83c52a10757b
Thiago Pires <nitrox2thi@gmail.com>
2018-09-25 22:40:04 -0300

    alteração nome de tabela

--- app/config.php
+++ app/config.php
@@ -1,20 +1,17 @@
 <?php
   $configs = new HXPHP\System\Configs\Config;
   ActiveRecord\Connection::$datetime_format = 'Y-m-d H:i:s';
-
   //Globais
   $configs->title = 'HXPHP Framework';
-
   //Configurações de Ambiente - Desenvolvimento
   $configs->env->add('development');
-
   $configs->env->development->baseURI = '/sistema/';
-
-  $configs->env->development->database->setConnectionData(array(
+  $configs->env->development->database->setConnectionData([
     'host' => 'localhost',
     'user' => 'root',
-    'password' => '',
-    'dbname' => 'sistemhx'
-  ));
+    'password' => 'root',
+    'dbname' => 'systemhx',
+    'charset' => env('MYSQL_CHARSET', 'utf8')
+  ]);
 
-return $configs;
+  return $configs;
